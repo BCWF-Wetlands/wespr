1f27a590eee4d60dcee1decb668ff3f2217bc179
Andy Teucher <andy.teucher@gmail.com>
2023-10-27 11:18:02 -0700

    Start on validator functions

--- R/functions.R
+++ R/functions.R
@@ -25,9 +25,69 @@ names_from_value <- function(x, value) {
 }
 
 record_values <- function(questions, data) {
-  lapply(questions, function(x) {
-    values <- data$site_1[data$q_no == x$name]
-    x$value <- values
-    x
+  lapply(questions, function(question) {
+    values <- data$site_1[data$q_no == question$name]
+    question$value <- question$validator(values)
+    question
   })
 }
+
+## Validators 
+## These ensure that the values being input from the questions are of
+## the right type and length, and within the right range. They are used in the
+## `record_values()` function
+
+multi_choice <- function(n) {
+  function(x) {
+    x <- as.numeric(x)
+    valid <- all(!is.na(x)) && 
+      all(x %in% 0:1) && 
+      sum(x) == 1 && 
+      length(x) == n
+    if (!valid) stop("Value must be length ", n, " and be all 0s and one 1")
+    as.logical(x)
+  }
+}
+
+multiresponse_binary <- function(n) {
+  function(x) {
+    x <- as.numeric(x)
+    valid <- all(!is.na(x)) && 
+      all(x %in% 0:1) && 
+      length(x) == n
+    if (!valid) stop("Value must be length ", n, " and be all 0s and 1s")
+    as.logical(x)
+  }
+}
+
+multiresponse_category <- function(n, min = -Inf, max = Inf) {
+  function(x) {
+    x <- as.numeric(x)
+    valid <- all(!is.na(x)) && 
+      all(x %in% min:max) && 
+      length(x) == n
+    if (!valid) stop("Value must be length ", n, " and be an integer from ", min, " to ", max)
+    x
+  }
+}
+
+numeric_value <- function(min, max) {
+  function(x) {
+    x <- as.numeric(x)
+    valid <- length(x) == 1 &&
+      !is.na(x) && 
+      x >= min && x <= max
+    if (!valid) stop("Value must be a single number between ", min, " and ", max)
+    x
+  }
+}
+
+logical_value <- function(x) {
+  x <- as.numeric(x)
+  valid <- length(x) == 1 &&
+    !is.na(x) &&
+    x %in% 0:1
+  if (!valid) stop("Value must be a single logical")
+  as.logical(x)
+}
+
