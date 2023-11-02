36b9ca3f912d987dff49d5e10e23064e2743d30a
Andy Teucher <andy.teucher@gmail.com>
2023-11-02 12:33:14 -0700

    Modify script so works with package

--- inst/script.R
+++ inst/script.R
@@ -1,27 +1,28 @@
 library(readr)
 library(dplyr)
-library(stringr)
-library(tidyr)
 
-source("R/setup.R")
+# Run devtools::load_all(), or in RStudio Ctrl+Shift+L to load the package
+# during development, or:
+# install_github("BCWF-wetlands/wespr)
+# library(wespr)
 
 # get weights table
-weights <- read_csv("input_data/weights.csv")
+weights <- read_csv("inst/input_data/weights.csv")
 
 # read in data and filter to questions we have implemented, and just one site:
-data <- load_wesp_data("input_data/wetflat.csv") |> 
-  select(q_no, response_no, site_1) |> 
+data <- load_wesp_data("inst/input_data/wetflat.csv") |>
+  select(q_no, response_no, site_1) |>
   filter(
     q_no %in% names(core_questions)
   )
 
-core_questions <- record_values(core_questions, data)
+core_questions <- record_values(data)
 
 derived_values <- derive_values(core_questions)
 
 fr_qs <- fn(core_questions, weights, fn = "FR")
 
-# Next: 
+# Next:
 # - Check valid values against template in list elements.
 #     - This should happen inside `record_values()`
 # - Add questions that create derived values (always_water etc) and generate
@@ -31,5 +32,5 @@ fr_qs <- fn(core_questions, weights, fn = "FR")
 #     - filter derived_values list by used_by
 #     - subset and match weights from weights table
 #     - ... magic
-# - For multi-site, I think each question in the core_questions object can have 
+# - For multi-site, I think each question in the core_questions object can have
 #   a list of values one for each site (rather than a questions object for each site)
