75dfd94ddb5eb9c1c37840b0160fd44ba038f1e2
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2023-11-26 16:49:15 -0800

    add prototye

---
+++ inst/cs_protype_calcs.R
@@ -0,0 +1,32 @@
+
+# CS calculation Prototype
+
+# Gen to populate with calculation in readable format.
+#
+# burned # OF15
+Burn6 = 1 - "OF15_1"
+
+
+# local moisure deficit - OF25_1
+
+
+
+# vegetation height & form diversity F1_0
+
+
+
+
+# woody Diameter Classes
+
+
+
+# dense Moss Extent
+
+
+# Percent Bare Ground
+
+
+# soil surface texture
+
+
+
