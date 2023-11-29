6ae4dcd4a74930e6fdef462e2d1e627ac373f472
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2023-11-28 17:53:59 -0800

    add more metrics for WS

--- inst/ws_protype_calcs.R
+++ inst/ws_protype_calcs.R
@@ -118,7 +118,7 @@ seaspct1 <- ifelse(NeverWater == 1, "", max(
 
 
 
-# F21 Pectent with Persistent Surface water
+# F21 Percent with Persistent Surface water
 
 permwpct1 <- ifelse(NeverWater == 1, "", max(
   F21_1 * WF21_1,
@@ -129,5 +129,219 @@ permwpct1 <- ifelse(NeverWater == 1, "", max(
   F21_6 * WF21_6
 ) /
   max(WF21_1, WF21_2, WF21_3, WF21_4, WF21_5, WF21_6))
+
+
+
+# F25 Surface water fluctuations
+#=IF((NeverWater=1),"",IF((NoPersis=1),"",MAX(F46:F50)/MAX(E46:E50)))
+
+fluctu1 <- ifelse(NeverWater == 1, "",
+                  ifelse(NoPersis == 1, "",
+                         max(F25_1 * WF25_1,
+                           F25_2 * WF25_2,
+                           F25_3 * WF25_3,
+                           F25_4 * WF25_4,
+                           F25_5 * WF25_5) /
+                           max(WF25_1,
+                               WF25_2,
+                               WF25_3,
+                               WF25_4,
+                               WF25_5)))
+
+
+#F27 Ponded water
+
+pondpct1 <- ifelse(NeverWater == 1, "",
+       ifelse(NoPersis == 1, "",
+              max(F27_1 * WF27_1,
+                  F27_2 * WF27_2,
+                  F27_3 * WF27_3,
+                  F27_4 * WF27_4,
+                  F27_5 * WF27_5,
+                  F27_6 * WF27_6) /
+                max(WF27_1,
+                    WF27_2,
+                    WF27_3,
+                    WF27_4,
+                    WF27_5,
+                    WF27_6)))
+
+
+# F29 - Largest Deep Ponded Water
+# =IF((NeverWater=1),"",IF((NoPersis=1),"",IF((NoPond=1),"",MAX(F59:F64)/MAX(E59:E64))))
+
+owareawet1 <- ifelse(NeverWater == 1, "",
+                 ifelse(NoPersis == 1, "",
+                        ifelse(NoPond == 1, "",
+                               max(F29_1 * WF29_1,
+                                   F29_2 * WF29_2,
+                                   F29_3 * WF29_3,
+                                   F29_4 * WF29_4,
+                                   F29_5 * WF29_5,
+                                   F29_6 * WF29_6) /
+                                 max(WF29_1,
+                                     WF29_2,
+                                     WF29_3,
+                                     WF29_4,
+                                     WF29_5,
+                                     WF29_6))))
+
+
+# F40 Channel connection and outflow duration
+#=IF((D69+D70>0), Outmap1,MAX(F66:F70)/MAX(E66:E70))
+
+outdura1 <- ifelse((F40_4 + F40_5 > 0),
+                   outmap1,
+                   max(
+                     F40_1 * WF40_1,
+                     F40_2 * WF40_2,
+                     F40_3 * WF40_3,
+                     F40_4 * WF40_4,
+                     F40_5 * WF40_5
+                   ) / max(WF40_1,
+                           WF40_2,
+                           WF40_3,
+                           WF40_4,
+                           WF40_5)
+)
+
+
+# F41 Outflow
+# =IF((NoOutlet+NoOutletX>0),"",IF((D75=1),"", MAX(F72:F74)/MAX(E72:E74)))
+# this is differnt to cs version of this formular
+
+constric1 <- #ifelse((NeverWater + TempWet) > 0, "",
+                    ifelse((NoOutlet + NoOutletX) > 0, "",
+                           ifelse(F41_4 == 1, "",
+                                  max(F41_1 * WF41_1,
+                                      F41_2 * WF41_2,
+                                      F41_3 * WF41_3) /
+                                    max(WF41_1, WF41_2, WF41_3)
+                           )
+                    )
+#)
+
+# F43 - Thoughflow Resistance
+#=IF(OR(Inflow=0,NoOutlet+NoOutletX>0),"",MAX(F77:F81)/MAX(E77:E81))
+
+throflo1 <- ifelse(Inflow == 0 | (NoOutlet + NoOutletX) > 0, "", max(
+    F43_1 * WF43_1,
+    F43_2 * WF43_2,
+    F43_3 * WF43_3,
+    F43_4 * WF43_4,
+    F43_5 * WF43_5
+  ) / max(WF43_1,
+          WF43_2,
+          WF43_3,
+          WF43_4,
+          WF43_5))
+
+
+# F44 Internal Gradient
+#=IF((NoOutlet+NoOutletX>0),"",IF((Inflow=1),"",MAX(F83:F86)/MAX(E83:E86)))
+
+gradient1 <- ifelse((NoOutlet + NoOutletX) > 0, "",
+                    ifelse(Inflow == 1, "",
+                           max(
+                             F44_1 * WF44_1,
+                             F44_2 * WF44_2,
+                             F44_3 * WF44_3,
+                             F44_4 * WF44_4
+                           ) / max(WF44_1,
+                                   WF44_2,
+                                   WF44_3,
+                                   WF44_4)))
+
+# F47 - gound water input probability
+
+groundw1 <- max(
+  F47_1 * WF47_1,
+  F47_2 * WF47_2,
+  F47_3 * WF47_3
+) / max(WF47_1,
+        WF47_2,
+        WF47_3)
+
+
+
+#######################################################
+## Overall WS Function  score
+
+## sub - function scores
+
+#=AVERAGE(Groundw1,SoilTex1,WetPctRCA1,GrowDays1)
+#=IF((NeverWater=1), "", AVERAGE(SeasPct1,Fluctu1))
+#=IF((NeverWater=1), AVERAGE(Gradient1,Girreg1,Gcover1), AVERAGE(Gradient1,Constric1,ThruFlo1, FloDist1, PondPct1))
+
+subsurf <- mean(c(groundw1,soiltex1,wetpctrca1,growdays1))
+livestore <- ifelse(NeverWater == 1, "", mean(c(seaspct1, fluctu1), na.rm = TRUE))
+friction <- ifelse(NeverWater == 1,
+                   mean(c(gradient1, girreg1, gcover1), na.rm = TRUE),
+                             mean(c(gradient1, constric1, thruflo1, flodist1, pondpct1), na.rm = TRUE))
+
+# final function score
+
+#=10*(IF((NoOutlet=1),1, IF((NeverWater=1),AVERAGE(MAX(Outmap1,OutDura1), AVERAGE(Friction, Subsurf)),AVERAGE(OutDura1, ((4*LiveStore+2*Friction+Subsurf)/7)))))
+ws_func_score <- 10 * ifelse(NoOutlet + NoOutletX >1 , 1,
+                        ifelse(NeverWater == 1,
+                             mean(c(max(outmap1, outdura1), mean(c(friction, subsurf), na.rm = TRUE)), na.rm = TRUE),
+                             mean(c(outdura1, ((4 * livestore + 2 * friction + subsurf) / 7)), na.rm = TRUE)
+                                )
+                              )
+
+
+
+#########################################################
+
+## Benefit
+
+# OF5 Relative elevation
+elev1v <- OF5_1
+
+# OF7 Aspect
+
+aspect1 <- max(
+  OF7_1 * WOF7_1,
+  OF7_2 * WOF7_2,
+  OF7_3 * WOF7_3
+) / max(WOF7_1,
+        WOF7_2,
+        WOF7_3)
+
+
+# OF9 - Floodable infrastucture
+#=IF((NoOutlet+ NoOutletX>0),"",MAX(F98:F101)/MAX(E98:E101))
+
+floodprop1v <- ifelse((NoOutlet + NoOutletX) > 0, "",
+                      max(OF9_1 * WOF9_1,
+                          OF9_2 * WOF9_2,
+                          OF9_3 * WOF9_3,
+                          OF9_4 * WOF9_4,) / max(WOF9_1,
+                                                 WOF9_2,
+                                                 WOF9_3,
+                                                 WOF9_4))
+
+
+# OF12 - unvegetated surface in the wetlands WAU
+#=IF((D15=1),"",MAX(F103:F105)/MAX(E103:E105))
+
+
+### UP TO HERE
+
+impervCA1 <- ifelse(D15 == 1, "", max(F103:F105) / max(E103:E105))
+
+
+
+
+
+
+# OF21 Local moisture defecit
+
+# OF 30 Road Density Within AA's buffer
+
+# OF41 Disturbed Percentage in the WAU
+
+# OF42 Road Density in the WAU
+
 
 
