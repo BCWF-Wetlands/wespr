8063b65399ba73c77037192e223c3462860bd39c
Andy Teucher <andy.teucher@gmail.com>
2023-12-08 12:01:04 -0800

    Placeholder function to run all indicators

---
+++ R/calc-indicators.R
@@ -0,0 +1,10 @@
+
+calc_indicators <- function(site) {
+  ws_f <- ws_f(site)
+  ws_b <- ws_b(site)
+  sr_f <- sr_f(site)
+  sr_b <- sr_b(site)
+  pr_f <- pr_f(site)
+  pr_b <- pr_b(site)
+  cs_f <- cs_f(site)
+}
