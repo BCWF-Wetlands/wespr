4b8ecce18d4af9c0e111e414009ed85dd8379f1f
Andy Teucher <andy.teucher@gmail.com>
2023-12-11 11:35:09 -0800

    Move question metadata and weightings to internal data

--- data-raw/wsp_calculations.R
+++ data-raw/wsp_calculations.R
@@ -4,22 +4,26 @@ library(dplyr)
 library(readr)
 library(janitor)
 
-# To authorize, set your google auth email with:
-# You can set in a project-specific .Rprofile file
+# Edit spreadsheet at:
+# https://docs.google.com/spreadsheets/d/1l2h7Z65H5z0cKv_gvorxkT6k9LC7ZKLS/edit#gid=924841838
+#
+# To authorize the download, set your google auth email with:
 # options(
 #   gargle_oauth_email = "email.which.gives.you.access.to.these.files@gmail.com"
 # )
+# You can add this to a project-specific .Rprofile file
 
 xl <- drive_find("WSP_calculations.xlsx")
 
 tmp_xl_gs <- drive_cp(xl, name = "tmp-wsp-calcs", mime_type = drive_mime_type("spreadsheet"))
 
-all_indicators <- read_sheet(tmp_xl_gs, sheet = "all_indicators", col_types = "c",
+question_metadata <- read_sheet(tmp_xl_gs, sheet = "all_indicators", col_types = "c",
                              .name_repair = make_clean_names) |>
-  filter(!is.na(no)) |>
-  select(-starts_with("x"))
+  filter(!is.na(no), no != "score") |>
+  mutate(n_responses = as.integer(n_responses)) |>
+  select(-starts_with("x"), -no_indicators)
 
-weightings <- read_sheet(
+indicator_weightings <- read_sheet(
   tmp_xl_gs,
   sheet = "weightings",
   .name_repair = make_clean_names,
@@ -28,8 +32,12 @@ weightings <- read_sheet(
 
 drive_rm(tmp_xl_gs)
 
-write_csv(all_indicators, "inst/input_data/all_indicators.csv")
-write_csv(weightings, "inst/input_data/weightings.csv")
+# write_csv(all_indicators, "inst/input_data/all_indicators.csv")
+# write_csv(weightings, "inst/input_data/weightings.csv")
 
-#
-# usethis::use_data(wsp_calculations, overwrite = TRUE)
+usethis::use_data(
+  question_metadata,
+  indicator_weightings,
+  internal = TRUE,
+  overwrite = TRUE
+)
