a6db6f9c5add0e0ff9909b2864fd0fef5ac2146e
Andy Teucher <andy.teucher@gmail.com>
2023-12-11 12:28:05 -0800

    Add placeholder functions for indicator calculations

--- R/calc-indicators.R
+++ R/calc-indicators.R
@@ -1,10 +1,37 @@
 
 calc_indicators <- function(site) {
-  ws_f <- ws_f(site)
-  ws_b <- ws_b(site)
-  sr_f <- sr_f(site)
-  sr_b <- sr_b(site)
-  pr_f <- pr_f(site)
-  pr_b <- pr_b(site)
-  cs_f <- cs_f(site)
+  site <- ws_f(site)
+  site <- ws_b(site)
+  # site <- sr_f(site)
+  # site <- sr_b(site)
+  # site <- pr_f(site)
+  # site <- pr_b(site)
+  site <- cs_f(site)
+  # site <- fr_f(site)
+  # site <- fr_b(site)
+  # site <- sens_f(site)
+  # site <- str_f(site)
+  # site <- nr_f(site)
+  # site <- nr_b(site)
+  # site <- ap_f(site)
+  # site <- pd_f(site) # requires AP_f
+  # site <- kmh_f(site) # requires AP_f * check ref with Paul as unclear
+  # site <- kmh_b(site)
+  # site <- wb_f(site)  # requires AP_f
+  # site <- wb_b(site)
+  # site <- pol_f(site) # requires PD_f
+  # site <- pol_b(site)
+  # site <- rsb_f(site) # requires AP_f, PD_f
+  # site <- rsb_b(site)
+  # site <- pd_b(site) # requires POL_f, RSB_f
+  # site <- oe_f(site) # requires AP_f , CS_f
+  # site <- am_f(site) # requires AP_f ,
+  # site <- am_b(site) # requires WB_f
+  # site <- fh_f(site) # requires AP_f,
+  # site <- fh_b(site) # requires WB_f
+  # site <- sfts_f(site)
+  # site <- sfts_b(site) # requires FH_f
+  # site <- ap_b(site) # requires FH_f , AH_f, WB_f, RSB_f
+  # site <- cri_b(site) # requires WB_f, FH_f
+  site
 }
