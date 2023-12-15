36b925bd0038c4c79e1a5dbb704a2f07a44cc355
Andy Teucher <andy.teucher@gmail.com>
2023-12-15 14:41:20 -0800

    Rename and start PR

---
+++ R/04_pr_fb.R
@@ -0,0 +1,17 @@
+
+# function
+pr_function <- function(site) {
+  indicator_data <- get_indicator_data(site, "pr")
+  vals <- get_vals(indicator_data)
+  weights <- get_weights(indicator_data)
+
+
+}
+
+
+# benefit
+pr_benefit <- function(site) {
+  indicator_data <- get_indicator_data(site, "pr")
+  vals <- get_vals(indicator_data)
+  weights <- get_weights(indicator_data)
+}
