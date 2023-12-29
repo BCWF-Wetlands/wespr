a87096d29c7fec3b0c055fc12046bc3f10a02353
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2023-12-29 12:00:42 -0800

    update draft comments

--- R/calc-indicators.R
+++ R/calc-indicators.R
@@ -24,7 +24,7 @@ calc_indicators <- function(site) {
   site <- update_site_indicator(site, "nr", "fun")
   site <- update_site_indicator(site, "nr", "ben")
   # site <- update_site_indicator(site, "ap", "fun") # draft complete - check the app is correct on site object
-  # site <- update_site_indicator(site, "pd", "fun") # requires ap_fun()
+  #site <- update_site_indicator(site, "pd", "fun") # draft complete - requires ap_fun()
   # site <- update_site_indicator(site, "kmh", "fun") # requires ap_fun() * check ref with Paul as unclear
   # site <- update_site_indicator(site, "kmh", "ben")
   # site <- update_site_indicator(site, "wb", "fun")  # requires ap_fun()
