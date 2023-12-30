a6cde66f1f9b07c109a0ba4996831ec3537f663d
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2023-12-29 16:45:23 -0800

    add function  calcs

---
+++ R/13_run_wb.R
@@ -0,0 +1,162 @@
+
+wb_fun <- function(site) {
+
+  indicator_data <- get_indicator_data(site, "wb")
+  vals <- get_vals(indicator_data)
+  weights <- get_weights(indicator_data)
+
+
+  distpond13 <- dist_to_ponded_water(vals, indicator_data)
+
+  distlake11 <- wt_max(indicator_data, "OF4", "fun")
+
+  elev11 <- 1 - vals$OF5_1
+
+  pctlakes11 <- wt_max(indicator_data, "OF18", "fun")
+
+  lakewet11 <- wt_max(indicator_data, "OF19", "fun")
+
+  fish11 <- ifelse(vals$OF20_5 == 1, 0 , NA_real_)
+
+  wb_rare11 <- ifelse(vals$OF24_1 == 1, 0 , NA_real_)
+
+  degreed11 <- degree_days_index(vals)
+
+
+  # TO do - check the range in this calcualtion
+  # https://github.com/BCWF-Wetlands/wespr/issues/52
+  herbscape13 <- if(sum_na (XXXXXXX) == 0){
+    NA_real_
+  } else {
+    wt_max(indicator_data, "OF43", "fun")
+  }
+
+
+  # TO do - check the range in this calcualtion
+  # https://github.com/BCWF-Wetlands/wespr/issues/52
+  wetdensWAU13 <-  if(sum_na (XXXXXXX) == 0){
+    NA_real_
+  } else {
+    wt_max(indicator_data, "OF43", "fun")
+  }
+
+  snags13 <- wt_max(indicator_data, "F8", "fun")
+
+  mudflat13 <- wt_max(indicator_data, "F16", "fun")
+
+  permwpct13 <- persist_water(vals, indicator_data)
+
+  fringe13 <- if (vals$NeverWater == 1 ||
+                  vals$NoPersis == 1||
+                  vals$F22_1 == 0 ){
+    NA_real_
+  } else {
+    1
+  }
+
+  lake13 <- if (vals$NeverWater == 1 ||
+                vals$NoPersis == 1||
+                vals$F23_1 == 0 ){
+    NA_real_
+  } else {
+    1
+  }
+
+  pondedpct13 <- if (vals$NeverWater == 1){
+    NA_real_
+  } else {
+    wt_max(indicator_data, "F27", "fun")
+  }
+
+  maxpondarea13 <- if((vals$NeverWater == 1) ||
+       vals$NoPond == 1 ||
+       vals$NoPersis == 1 ) {
+      NA_real_
+    } else {
+      wt_max(indicator_data, "F29", "fun")
+    }
+
+
+  maxpondpct13 <- if((vals$NeverWater == 1) ||
+                     vals$NoPond == 1 ||
+                     vals$NoPersis == 1 ) {
+    NA_real_
+  } else {
+    wt_max(indicator_data, "F30", "fun")
+  }
+
+
+  owarea13 <- open_water_extent(vals, indicator_data)
+
+  fetch13 <- distance_across_longest_openwater(vals, indicator_data)
+
+  widthdry13 <- distance_open_water_upland_veg_3(vals, indicator_data)
+
+  interspers13 <- interspersion_inundated_veg_1(vals, indicator_data)
+
+  steepshore13 <- if(vals$NeverWater == 1 ||
+                     vals$NoDeepPonded == 1 ||
+                     vals$NoOW == 1 ||
+                     vals$NoPersis == 1 ) {
+    NA_real_
+  } else {
+    wt_max(indicator_data, "F36", "fun")
+  }
+
+  cttail13 <- inundated_erect_veg(vals, indicator_data)
+
+  sav13 <- submerged_floating_aquatics(vals, indicator_data)
+
+  gradient13 <- internal_gradient(vals, indicator_data)
+
+  beaver13 <- wt_max(indicator_data, "F48", "fun")
+
+  perimcov13 <- vegetation_buffer_along_permin(vals, indicator_data)
+
+  rarebird11 <- ifelse(vals$F58_9 == 1, 1, NA_real_)
+
+  noise11 <- vals$S6_subscore
+
+
+  #todo: find better way to select calculations of app.
+  #appscore11 <-
+
+  # function subscore
+
+  lscape13 <- mean_na(perimcov13,
+                      mean_na(distpond13, distlake11, lakewet11, pctlakes11, wetdensWAU13),
+                      elev11, noise11, herbscape13)
+  hydro13 <- mean_na(gradient13, pondedpct13, permwpct13, lake13, degreed11)
+
+  produc13 <- appscore11
+
+  struc13 <- (mean_na(mudflat13, cttail13, sav13, interspers13) +
+             mean_na(maxpondpct13, fringe13, steepshore13, fetch13, widthdry13, fish11, beaver13, snags13))/2
+
+
+
+  wb_fun_score <- ifelse(vals$NeverWater == 1, 0,
+                   10 * (3 * max(c(mean(c( pondedpct13, owarea13, maxpondarea13)), wb_rare11, rarebird11) +
+                                   2 * mean(c(hydro13, struc13, produc13, lscape13))) / 5))
+
+  wb_fun_score
+
+
+}
+
+
+# benefits
+
+wb_ben <- function(site) {
+
+  indicator_data <- get_indicator_data(site, "wb")
+  vals <- get_vals(indicator_data)
+  weights <- get_weights(indicator_data)
+
+
+  # UP to here....
+
+  wb_ben_score
+
+}
+
