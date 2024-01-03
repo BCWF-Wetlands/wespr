d78ea64a682f7eb392c9af83501a73085b99726e
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2024-01-02 16:48:05 -0800

    add text

--- R/calc-indicators.R
+++ R/calc-indicators.R
@@ -34,10 +34,10 @@ calc_indicators <- function(site) {
   site <- update_site_indicator(site, "rsb", "fun") # draft complete - requires ap_fun(), pd_fun()
   site <- update_site_indicator(site, "rsb", "ben")
   site <- update_site_indicator(site, "pd", "ben") # requires pol_fun(), rsb_fun()
-  # site <- update_site_indicator(site, "oe", "fun") # requires ap_fun() , cs_fun()
-  # site <- update_site_indicator(site, "am", "fun") # requires ap_fun() ,
-  # site <- update_site_indicator(site, "am", "ben") # requires wb_fun()
-  # site <- update_site_indicator(site, "fh", "fun") # requires ap_fun(),
+  site <- update_site_indicator(site, "oe", "fun") # draft complete - requires ap_fun() , cs_fun()
+  site <- update_site_indicator(site, "am", "fun") # requires ap_fun() ,
+  site <- update_site_indicator(site, "am", "ben") # requires wb_fun()
+  site <- update_site_indicator(site, "fh", "fun") # requires ap_fun(),
   # site <- update_site_indicator(site, "fh", "ben") # requires wb_fun()
   # site <- update_site_indicator(site, "sfts", "fun")
   # site <- update_site_indicator(site, "sfts", "ben") # requires fh_fun()
