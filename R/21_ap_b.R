621a0283c7b497e67c168da8732d5f08093c508e
Andy Teucher <andy.teucher@gmail.com>
2024-01-11 14:59:09 -0800

    Use get_indicator_score() in app_ben

--- R/21_ap_b.R
+++ R/21_ap_b.R
@@ -1,31 +1,22 @@
 
-ap_ben <- function(site) {
+app_ben <- function(site) {
 
-  indicator_data <- get_indicator_data(site, "ap")
+  indicator_data <- get_indicator_data(site, "app")
   vals <- get_vals(indicator_data)
-  weights <- get_weights(indicator_data)
-  ind_scores <- get_indicator_scores(site)
-
 
   elev8v <- if(vals$NoOutlet == 1) {
-      NA_real_
+    NA_real_
   } else {
     vals$OF5_1
-      }
+  }
 
+  fscorefh8v <- get_indicator_score(site, "fh", "fun") / 10
 
-  # UP TO HERE ..............
+  fscoream8v <- get_indicator_score(site, "am", "fun") / 10
 
-  # to do : check how to retrieve the final score
-  # is there a more elegant way to extract these
+  fscorewb8v <- get_indicator_score(site, "wb", "fun") / 10
 
-  # fscorefh8v <- ind_scores$indicator
-
-  #fscoream8v <-
-
-  #fscorewb8v <-
-
-  #fscoreSBM8v <-
+  fscoreSBM8v <- get_indicator_score(site, "rsb", "fun") / 10
 
 
   ap_ben_score <- 10 * (mean_na (elev8v,
