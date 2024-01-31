c62c4f4df5ecc5c836039dc1c1a17a6b72b0357f
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2024-01-31 12:05:05 -0800

    fix final cals

--- R/20_sfts_fb.R
+++ R/20_sfts_fb.R
@@ -180,6 +180,19 @@ sfts_ben<- function(site) {
                             mean_na(c(wetdef2, gdd2v, solar2v, glacier2v, aspect2v)) +
                             mean_na(c(perminpctper2v, impervrca2v, rddens2v, rddenswau2v, flowalt2, disturb2v)))/6)
 
+  library(dplyr)
+
+  sfts_ben_score <- 10 * max_na(outmap2, outdura2)  *
+    (3 * fishscore2v +
+      mean_na(elev2v, wetpctrca2v) +
+      mean_na(c(wetdef2, gdd2v, solar2v, glacier2v, aspect2v)) +
+      mean_na(c(perminpctper2v, impervrca2v, rddens2v, rddenswau2v, flowalt2, disturb2v))
+  ) / 6
+
+
+
+
+
 
   as.indicator_score(sfts_ben_score)
 }
