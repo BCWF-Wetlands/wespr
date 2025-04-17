eb577c57438f40d5b1bdf059d59046824d64d1c4
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2025-04-17 13:45:23 -0700

    update test file

--- R/test-helpers.R
+++ R/test-helpers.R
@@ -1,5 +1,5 @@
 make_test_site <- function(site = NULL, quiet = TRUE) {
-  data <- load_wesp_data(testthat::test_path("test-wetflat_20250417.csv"))
+  data <- load_wesp_data(testthat::test_path("test-wetflat.csv"))
 
   f <- if (quiet) suppressMessages else identity
   f(
