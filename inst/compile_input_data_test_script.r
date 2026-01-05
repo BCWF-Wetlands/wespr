53b572d1b79dd72a6bc8923d5b9c77b0569b023f
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2026-01-05 14:35:29 -0800

    update internal script

--- inst/compile_input_data_test_script.r
+++ inst/compile_input_data_test_script.r
@@ -1,7 +1,7 @@
 ## TESTING AND DEVELOPMENT SCRIPT
 
 # This is the testing and development script to use to test the combine_rawdat function.
-# Use is for internal and develpoment only and not for pacakge use.
+# Use is for internal and development only and not for pacakge use.
 
 # load libraries
 library(dplyr)
@@ -114,10 +114,16 @@ write.csv(wespEcoS, fs::path(out_dir, out_name),row.names=FALSE)
 ###########################################################################
 
 library(wespr)
+library (fs)
+
+#field_data <- fs::path("inst",'input_data','raw',"20251217", "field_assessment_by_class_20Nov2025.csv")
+#desktop_data <- fs::path("inst",'input_data','raw', "20251217", "office_data_by_class_formatted_for_R_12Dec2025.csv")
 
 field_data <- fs::path("inst",'input_data','raw',"20251204", "WESP_FIELD_MA.csv")
 desktop_data <- fs::path("inst",'input_data','raw', "20251204", "WESP_DESKTOP_MA.csv")
 
+#field_data <- fs::path("inst",'input_data','raw',"20251222", "WESP_Field_AE2.csv")
+#desktop_data <- fs::path("inst",'input_data','raw', "20251222", "WESP_Desktop_AE2.csv")
 
 ww <- format_rawinputs(
   field_data <- field_data,
@@ -127,9 +133,9 @@ ww <- format_rawinputs(
   overwrite = TRUE
 )
 
-write.csv(ww, fs::path("inst",'input_data','raw',"20251204","wesp_input_20251204.csv"), row.names=FALSE)
+write.csv(ww, fs::path("inst",'input_data','raw',"20251222","wesp_input_20251222.csv"), row.names=FALSE)
 
-indata <- fs::path("inst",'input_data','raw',"20251204","wesp_input_20251204.csv")
+indata <- fs::path("inst",'input_data','raw',"20251222","wesp_input_20251222.csv")
 
 check_indata(indata)
 
@@ -167,8 +173,84 @@ ind_scores
 get_responses(site)
 get_derived_values(site)
 
-out <- assign_jenks_score(ind_scores, calibration_scores, EcoP = "GD", report = FALSE, output_dir = "temp")
+out <- assign_jenks_score(ind_scores, calibration_scores, EcoP = "GD", report = TRUE, output_dir = "temp")
 
-write.csv(out, fs::path("inst",'input_data','raw',"20251204","wesp_output_20251204.csv"), row.names=FALSE)
+write.csv(out, fs::path("inst",'input_data','raw',"20251222","wesp_output_20251222.csv"), row.names=FALSE)
+
+
+
+
+
+#Preparing the raw data
+# check old data format from contractors
+
+field_data <- fs::path("inst",'input_data','raw',"20251217", "field_assessment_by_class_20Nov2025.xls")
+office_data <- fs::path("inst",'input_data','raw', "20251217", "office_data_by_class_formatted_for_R_12Dec2025.xlsx")
+
+
+ww <- combine_rawdata(
+  field_data <-  field_data ,
+  office_data <- office_data,
+  EcoP = "GD",
+  write_subfiles = FALSE,
+  out_dir <- "inst/input_data/raw/20251217",
+  overwrite = TRUE
+)
+
+ww$Question
+
+write.csv(ww, fs::path("inst/input_data/reference_SIM_20250620.csv"), row.names=FALSE)
+write.csv(ww, fs::path("inst/input_data/reference_GD_20250620.csv"), row.names=FALSE)
+
+
+
+#indata <- fs::path("inst/input_data/wetFlat_20250417.csv")
+indata <- fs::path("inst/input_data/reference_SIM_20250620.csv")
+indata <- fs::path("inst/input_data/reference_GD_20250620.csv")
+
+#check_indata(indata)
+
+wesp_data <- load_wesp_data(indata)
+site <- as.wesp_site(wesp_data)
+
+
+calculate_jenks_score(wesp_data, out_dir = "temp",  out_name = "wesp_scores_GD.csv")
+calculate_jenks_score(wesp_data, out_dir = "temp",  out_name = "wesp_scores_SIM.csv")
+
+
+
+
+
+# check old file
+wesp_file <- system.file("input_data/wetFlat_20240130.csv", package = "wespr")
+wesp_data <- load_wesp_data(wesp_file)
+head(wesp_data)
+
+wesp_file <- read.csv("inst/input_data/wetFlat_20240130.csv")
+#setdiff(names(wesp), names(wesp_file))
+setdiff(wesp$Question, wesp_file$Question)
+#[1] "F4_4" (error fix)
+# "F57_7"  (error fix)
+#"OF15"       "OF16"       "OF17"       "OF21"       "OF22"  (were previously _1)
+
+setdiff(wesp_file$Question, wesp$Question)
+#[1] "F22_1"  "F23_1"  "F42_1"
+#"F49_1"  #"OF15_1" "OF16_1" "OF17_1" "OF21_1" "OF22_1"
+
+
+
+
+
+# run the data with new data
+wesp <- fs::path("inst/input_data/wetFlat_202503241.csv")
+wesp_data <- load_wesp_data(wesp)
+
+# 2)  run all sites
+site <- as.wesp_site(wesp_data, site = 1)
+calculate_jenks_score(wesp_data, out_dir = "temp",  out_name = "wesp_scores_test1.csv")
+
+#Write out the data frame
+write.csv(wespEcoS, fs::path(out_dir, out_name),row.names=FALSE)
+
 
 
