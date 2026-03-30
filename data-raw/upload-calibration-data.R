295fb449e28251bb51bf71aca559fed17093f4f5
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2026-03-30 10:15:46 -0700

    update internal cal data

--- data-raw/upload-calibration-data.R
+++ data-raw/upload-calibration-data.R
@@ -11,37 +11,28 @@ library(readxl)
 library(dplyr)
 
 
-
-# 2. read in the preprocessed file.
-# Note this assumes the data has been preprossed into the standard format to be
+# 2. read in the prepossessed file.
+# Note this assumes the data has been prepossessed into the standard format to be
 # uploaded into the wespr calculator.
 
 # See https://bcwf-wetlands.github.io/wespr/articles/prepare-rawdata.html for details on this
 # step.
 
 # update this line to point to specific file location
-#indata <- fs::path("inst/input_data/input_refsites_GD.csv")
-indata <- fs::path("inst/input_data/reference_SIM_20250620.csv")
-indata <- fs::path("inst/input_data/reference_GD_20250620.csv")
+
+#indata <- fs::path("inst/input_data/reference_SIM_20250620.csv"); ecop <- "SIM"
+#indata <- fs::path("inst/input_data/reference_GD_20250620.csv"); ecop <- "GD"
+#indata <- fs::path("inst/input_data/reference_SI_20260319.csv"); ecop <- "SI"
+#indata <- fs::path("inst/input_data/reference_SBI_20260319.csv"); ecop <- "SBI"
+#indata <- fs::path("inst/input_data/reference_TBP_20260319.csv"); ecop <- "TBP"
+#indata <- fs::path("inst/input_data/reference_CI_20260319.csv");  ecop <- "CI"
 
 
-
-# to add
-indata <- fs::path("inst/input_data/reference_SI_20260319.csv")
-indata <- fs::path("inst/input_data/reference_SBI_20260319.csv")
-indata <- fs::path("inst/input_data/reference_TBP_20260319.csv")
-indata <- fs::path("inst/input_data/reference_CI_20260319.csv")
-
-
-
-
-ecop <- "GD"
-
-
-
-# load in the data
+# Read in data into wesp format
 wesp_data <- load_wesp_data(indata)
-site <- as.wesp_site(wesp_data)
+
+# generate a site key
+wespkey <- wespr::generate_ids(wesp_data)
 
 # calculate the scores
 # note this script generates the jenks breaks on the normalised scores based on the 100 or so
@@ -50,55 +41,32 @@ site <- as.wesp_site(wesp_data)
 wespclass <- calculate_jenks_score(wesp_data, out_dir = "temp",  out_name = paste0("wesp_scores_", ecop, ".csv"))
 
 
-#export as full dataset - testing this option
-
-
 # add the Eco province name
 calibration_scores_new <- wespclass |>
-  dplyr::mutate(ecoprovince = ecop) |>
-  dplyr::select(-wetland_id)
+  dplyr::mutate(ecoprovince = ecop) #|>
+ # dplyr::select(-wetland_id)
+
+ calibration_scores_ci <- calibration_scores_new
+#calibration_scores_tbp <- calibration_scores_new
+# calibration_scores_sbi <- calibration_scores_new
+#calibration_scores_si <- calibration_scores_new
+#calibration_scores_gd <- calibration_scores_new
+#calibration_scores_sim <- calibration_scores_new
 
 
+calibration_scores <- dplyr::bind_rows(calibration_scores_gd, calibration_scores_sim)
+calibration_scores <- dplyr::bind_rows(calibration_scores, calibration_scores_si)
+calibration_scores <- dplyr::bind_rows(calibration_scores, calibration_scores_sbi)
+calibration_scores <- dplyr::bind_rows(calibration_scores, calibration_scores_tbp)
+calibration_scores <- dplyr::bind_rows(calibration_scores, calibration_scores_ci)
 
 
-
-# format output data to get the range of each category per service
-
-#wcols <- names(wespclass)
-## remove the site column
-#wcols <- wcols[!wcols %in% c("site", "wetland_id")]
-#wcols <- unique(sub("^([^_]*_[^_]*).*", "\\1", wcols))
-
-# loop through the data and get the min and max values of the raw scores
-
-# outsum <- purrr::map(wcols, function(x) {
-#   # get the columns for each service
-#  # x <- wcols[1]
-#
-#   tw <- wespclass |>
-#     dplyr::select(dplyr::starts_with(x)) |>
-#     dplyr::select(-dplyr::ends_with("_norm"))
-#   names(tw) <- c("jenks", "raw" )
-#
-#   tww <- tw |>
-#     dplyr::group_by(jenks) |>
-#     dplyr::summarise(min = min(raw),
-#                      max = max(raw)) |>
-#     dplyr::mutate(service = x,
-#            service_name = unique(sub("^([^_]*).*", "\\1", service)),
-#            service_type = unique(sub("^[^_]*_", "", service)))
-#   tww
-#
-# }) |>  dplyr::bind_rows()
-
-# add the Eco province name
-#calibration_scores_new <- outsum |>
-#  dplyr::mutate(ecoprovince = ecop) |>
-#  dplyr::select(ecoprovince, service, everything())
+check <- calibration_scores |>
+  group_by(ecoprovince) |>
+             count()
 
 
-# write out (temp fix while testing)
-#write.csv(calibration_scores_new, "temp/sim_jenks_breaks.csv", row.names = FALSE)
+write.csv(calibration_scores, fs::path("temp", "wesp_scores_all.csv"))
 
 
 # if data already exists then read in first and merge or updata
@@ -129,9 +97,11 @@ if(!missing(calibration_scores)){
 #calibration_scores = NA
 # export data as part of the package
 
+calibration_scores
+
 # convert to internal data
 usethis::use_data(
   calibration_scores,
-  internal = FALSE,
+  internal = TRUE,
   overwrite = TRUE
 )
