b4d98bb677545cb091dee73f4fb3bb75e7cf795a
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2026-05-04 21:34:15 -0700

    add function to build report

---
+++ R/build_report.R
@@ -0,0 +1,221 @@
+
+#' Build Site Report
+#'
+#' @param ind_scores A data.frame of indicator scores. The output of get_indicator_scores().
+#' @param calibration_scores an internal dataset containing the calibration data for all sites. This can be updated by admin.
+#' @param EcoP A character string specifying the region. Default = 'GD'
+#' @param output_dir A character string specifying the directory to save the report. Default = NULL.
+#'
+#' @returns A data.frame with indicator scores and jenks classification score (Low, Medium, High (L, M, H)).
+#' @export
+#' @examples
+#' \dontrun{
+#' wesp <- fs::path("inst/input_data/wetFlat_20250427_single.csv")
+#' wesp_data <- load_wesp_data(wesp)
+#' site <- as.wesp_site(wesp_data)
+#' site <- calc_indicators(site)
+#' ind_scores <- get_indicator_scores(site)
+#' out <- assign_jenks_score(ind_scores, calibration_scores, EcoP = "GD")
+#' out <- build_report(ind_scores, calibration_scores, EcoP = "GD", output_dir = "temp")
+#' }
+build_report <- function(ind_scores, calibration_scores, EcoP, output_dir = NULL) {
+
+  # ## testing lines
+  # ind_scores
+  # calibration_scores
+  # EcoP = "SIM"
+  # output_dir = "temp"
+
+  # check calibration data contains ecoprovince
+  if (!EcoP %in% unique(calibration_scores$ecoprovince)) {
+    cli::cli_abort("No calibration data is currently stored for the selected Eco Province.
+                   Please check your ecoprovince name or select from the following:
+                   {unique(calibration_scores$ecoprovince)}")
+  }
+
+  # check output file folder exisits
+  if (!dir.exists(fs::path(output_dir))) {
+    cli::cli_abort("The selected output folder:{ vars output_dir} does not exist, please check and choose a folder that exists")
+  }
+
+  # check wetland_id is in the dataset
+  if (!"wetland_id" %in% names(ind_scores)) {
+    cli::cli_abort("There is no wetland id associated with the input data, please check and add this column for reporting")
+  }
+
+
+  #build site key to make sure the value is added to report
+  site_key <- ind_scores |>
+    dplyr::select( .data$site,  .data$wetland_id) |>
+    unique()
+
+
+  # format ind_scores to long format
+
+  ind <- ind_scores |>
+    dplyr::select(.data$site, .data$indicator, .data$fun) |>
+    dplyr::mutate(service_type = "f") |>
+    dplyr::rename("value" = .data$fun)
+
+  indb <- ind_scores |>
+    dplyr::select(.data$site, .data$indicator, .data$ben) |>
+    dplyr::mutate(service_type = "b") |>
+    dplyr::rename("value" = .data$ben)
+
+  ind <- rbind(ind, indb)
+
+  calibration_scores_eco <- calibration_scores |>
+    dplyr::filter(.data$ecoprovince == EcoP)
+
+  # format the calibration scores to summarise to get the range of each category per service
+
+  wcols <- names(calibration_scores_eco)
+  ## remove the site column
+  wcols <- wcols[!wcols %in% c("site", "wetland_id", "ecoprovince")]
+  wcols <- unique(sub("^([^_]*_[^_]*).*", "\\1", wcols))
+
+  # get the highest and lowest raw scores across all sites for standardizing value
+  raw_range_scores <- calibration_scores_eco
+
+  min_raw_cal <- as.data.frame(sapply(calibration_scores_eco, min, na.rm = TRUE))
+  names(min_raw_cal) <- "min"
+
+  max_raw_cal <- as.data.frame(sapply(calibration_scores_eco, max, na.rm = TRUE))
+  names(max_raw_cal) <- "max"
+
+  min_max_raw_cal <- cbind(min_raw_cal, max_raw_cal)
+  min_max_raw_cal$names <- row.names(min_max_raw_cal)
+  min_max_raw_cal <- tibble::as_tibble(min_max_raw_cal)
+
+  raw_names <- grep("_raw", min_max_raw_cal$names, value = T)
+
+  min_max_raw_cal <- min_max_raw_cal |>
+    dplyr::filter(names %in% raw_names)
+
+
+  # loop through the data and get the min and max values of the raw scores
+
+  outsum <- purrr::map(wcols, function(x) {
+    # get the columns for each service
+    # x <- wcols[1]
+
+    tw <- calibration_scores_eco |>
+      dplyr::select(dplyr::starts_with(x)) |>
+      dplyr::select(-dplyr::ends_with("_norm"))
+    names(tw) <- c("jenks", "raw")
+
+    tww <- tw |>
+      dplyr::group_by(.data$jenks) |>
+      dplyr::summarise(
+        n = dplyr::n(),
+        min = min(raw),
+        max = max(raw)
+      ) |>
+      dplyr::mutate(
+        service = x,
+        service_name = unique(sub("^([^_]*).*", "\\1", .data$service)),
+        service_type = unique(sub("^[^_]*_", "", .data$service))
+      )
+    tww
+  }) |> dplyr::bind_rows()
+
+  # add the Eco province name
+  calibration_scores_summary <- outsum |>
+    dplyr::mutate(ecoprovince = EcoP) |>
+    dplyr::select(.data$ecoprovince, .data$service, dplyr::everything())
+
+  # loop through the ind_score data and find match for each row that corresponds with
+  # either L, M, H class in calibration value
+
+  classed_df <- lapply(1:nrow(ind), function(i) {
+    # print(i)
+    # i <- 20
+    trow <- ind[i, ]
+
+    # filter for the service and f/b
+    calr <- calibration_scores_summary |>
+      dplyr::filter(.data$service_name == trow$indicator) |>
+      dplyr::filter(.data$service_type == trow$service_type)
+
+    # get the highest and lowest raw value for standardization data across all sites
+    calr_min_max <- paste0(unique(calr$service), "_raw")
+    min_max_raw_cal_i <- min_max_raw_cal |> dplyr::filter(names == calr_min_max)
+    cal_raw_min <- as.numeric(min_max_raw_cal_i$min)
+    cal_raw_max <- as.numeric(min_max_raw_cal_i$max)
+
+    my_min <- trow$value
+    my_max <- trow$value
+
+    # is na then assign to NA
+    if (is.na(my_min)) {
+      cal_val <- NA
+      sta_val <- NA
+    } else {
+      my_df_filtered <- calr |>
+        dplyr::rowwise() |>
+        dplyr::filter(my_min >= .data$min & my_max <= .data$max)
+
+      if (nrow(my_df_filtered) == 0) {
+        # check if value is higher than H max or lower than min for L
+
+        calr_h <- calr |>
+          dplyr::filter(.data$jenks == "H") |>
+          dplyr::select(.data$max) |>
+          dplyr::pull()
+
+        if (my_max > calr_h) {
+          cal_val <- "H"
+        } else {
+          calr_l <- calr |>
+            dplyr::filter(.data$jenks == "L")
+
+          cal_val <- "L"
+        }
+
+        cli::cli_alert_warning("Value {round(trow$value,2)} for {trow$indicator} ({trow$service_type}) is outside the calibration range, assign to closest match")
+      } else {
+        cal_val <- my_df_filtered$jenks
+      }
+    }
+
+    if (!is.na(my_min)) {
+      # calculate the standardized value
+      sta_val <- dplyr::case_when(
+        trow$value < cal_raw_min ~ 0,
+        trow$value > cal_raw_max ~ 10,
+        .default = NA
+      )
+      if (is.na(sta_val)) {
+        sta_val <- 10 * ((trow$value - cal_raw_min) / (cal_raw_max - cal_raw_min))
+      }
+    }
+
+    trow |> dplyr::mutate(
+      calibration_scores_summary = cal_val,
+      standardized_score = sta_val
+    )
+  }) |> dplyr::bind_rows()
+
+
+  classed_df <- classed_df |>
+    dplyr::filter(!is.na(.data$value)) |>
+    dplyr::mutate(value = round(.data$value, 2),
+                  sta_value = round(.data$standardized_score,2)) |>
+    dplyr::select(.data$site, .data$indicator, .data$service_type, .data$value, .data$sta_value,.data$calibration_scores_summary )
+
+ # add site key
+  classed_df <- dplyr::left_join(classed_df, site_key, by = dplyr::join_by(site))
+
+
+  RMD <- system.file("extdata/site_report.rmd", package = "wespr")
+
+  rmarkdown::render(RMD,
+                    params = list(
+                      calibration_scores_eco = calibration_scores_eco,
+                      calibration_scores_summary = calibration_scores_summary,
+                      ecoprovince_sp = ecoprovince_sp,
+                      classed_df = classed_df
+                    ),
+                    output_dir = output_dir
+  )
+}
