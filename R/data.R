0c699c98999a4e718053de22dbfed80532e8d1c5
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2026-05-04 21:33:35 -0700

    update readme

--- R/data.R
+++ R/data.R
@@ -13,3 +13,20 @@
 #' }
 #' @source see developers
 "calibration_scores"
+
+
+#' #' WESP weights table
+#' #'
+#' #' This dataset is created for the purpose of reviewing weightings used within wespr
+#' #' The script to process this data is available : `internal-data.R`.
+#' #'
+#' #' @format ## `wt_table`
+#' #' A data frame with 2083 rows and 8 columns:
+#' #' \describe{
+#' #'   \item{ecoprovince}{ecoprovince abbreviation}
+#' #'   \item{service}{2 & 3 letter ISO country codes}
+#' #'   \item{year}{Year}
+#' #'   ...
+#' #' }
+#' #' @source see developers
+#' "wt_table"
